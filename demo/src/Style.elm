5d93b091837555f39d3993c2c2a140b3fa82cb76
James Carlson <jxxcarlson@mac.com>
2020-02-12 23:07:47 +0300

    Remove unneeded style elements

--- demo/src/Style.elm
+++ demo/src/Style.elm
@@ -1,56 +1,17 @@
-module Style exposing (editor, footer, innerContainer, outerContainer, renderedText)
+module Style exposing
+    ( grayColor
+    , redColor
+    )
 
-import Html.Attributes as HA
-
-
-outerContainer width height =
-    [ HA.style "padding-top" "0px"
-    , HA.style "width" (px width)
-    , HA.style "height" (px height)
-    , HA.style "display" "flex"
-    , HA.style "flex-direction" "column"
-    , HA.style "justify-content" "center"
-    , HA.style "overflow" "hidden"
-    ]
+import Element
 
 
-innerContainer width height =
-    [ HA.style "width" (px width)
-    , HA.style "height" (px height)
-    , HA.style "display" "flex"
-    , HA.style "flex-direction" "row"
-    , HA.style "justify-content" "center"
-    , HA.style "overflow" "hidden"
-    ]
+redColor =
+    Element.rgb255 150 40 40
 
 
-footer width height =
-    [ HA.style "width" (px width)
-    , HA.style "height" (px height)
-    , HA.style "display" "flex"
-    , HA.style "flex-direction" "row"
-    , HA.style "overflow" "hidden"
-    , HA.style "background-color" "#666"
-    ]
-
-
-renderedText width height =
-    [ HA.style "border" "solid"
-    , HA.style "border-width" "0.5px"
-    , HA.style "border-color" "#444"
-    , HA.style "width" (px (width - 40))
-    , HA.style "height" (px (height + 52))
-    , HA.style "overflow-y" "scroll"
-    , HA.style "padding-left" "18px"
-    , HA.style "padding-right" "18px"
-    ]
-
-
-editor width =
-    [ HA.style "border" "solid"
-    , HA.style "border-width" "0.5px"
-    , HA.style "border-color" "#444"
-    ]
+grayColor =
+    Element.rgb255 90 90 100
 
 
 px : Float -> String
