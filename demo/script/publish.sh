733b66c34e76c90388f5c2e492cdc68c3eaf191d
James Carlson <jxxcarlson@mac.com>
2020-02-12 19:55:45 +0300

    update publishing script

--- demo/script/publish.sh
+++ demo/script/publish.sh
@@ -1,6 +1,6 @@
 !#/bin/sh
 
-TARGET=/Users/carlson/dev/github_pages/app/janiczek-editor
+TARGET=/Users/carlson/dev/github_pages/app/text-editor
 DIST=public
 SOURCE=src
 
@@ -12,5 +12,12 @@ uglifyjs ${DIST}/Main.js -mc 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9"' -o ${DIST}/M
 echo "Copying ..."
 cp ${DIST}/index-min.html ${TARGET}/index.html
 cp ${DIST}/Main.min.js ${TARGET}/
+
+cp ${DIST}/assets/custom-element-config.js ${TARGET}/assets/custom-element-config.js
+cp ${DIST}/assets/math-text.js ${TARGET}/assets/math-text.js
+cp ${DIST}/assets/math-text-delayed.js ${TARGET}/assets/math-text-delayed.js
+cp ${DIST}/assets/outside.js ${TARGET}/assets/outside.js
+cp ${DIST}/assets/style.css ${TARGET}/assets/style.css
+
 
 echo "cd /Users/carlson/dev/github_pages/"
