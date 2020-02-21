74f397e5a0ff719a53b120761cc16615be53134f
James Carlson <jxxcarlson@mac.com>
2020-02-21 09:33:00 +0100

    Use Davide's new code for the custom elements

--- demo/public/assets/math-text-delayed.js
+++ demo/public/assets/math-text-delayed.js
@@ -1,21 +1,57 @@
-class MathTextDelayed extends HTMLElement {
+// Old version
 
-   // The "set content" code below detects the
-   // argument to the custom element
-   // and is necessary for innerHTML
-   // to receive the argument.
-   set content(value) {
-  		this.innerHTML = value
-  	}
+//class MathTextDelayed extends HTMLElement {
+//
+//   // The "set content" code below detects the
+//   // argument to the custom element
+//   // and is necessary for innerHTML
+//   // to receive the argument.
+//   set content(value) {
+//  		this.innerHTML = value
+//  	}
+//
+//  connectedCallback() {
+//    this.attachShadow({mode: "open"});
+//    this.shadowRoot.innerHTML =
+//      '<mjx-doc><mjx-head></mjx-head><mjx-body>' + this.innerHTML + '</mjx-body></mjx-doc>';
+//      // MathJax.typesetShadow(this.shadowRoot);
+//      setTimeout(() => MathJax.typesetShadow(this.shadowRoot), 1);
+//  }
+//}
+//
+//customElements.define('math-text-delayed', MathTextDelayed)
+
+// New version
+
+class MathText extends HTMLElement {
+
+  constructor(...args) {
+    super(...args);
+    this.attachShadow({mode: "open"});
+    this.shadowRoot.innerHTML = '<mjx-doc><mjx-head></mjx-head><mjx-body></mjx-body></mjx-doc>';
+    MathText.observer.observe(this, {childList: true});
+  }
 
   connectedCallback() {
-    this.attachShadow({mode: "open"});
-    this.shadowRoot.innerHTML =
-      '<mjx-doc><mjx-head></mjx-head><mjx-body>' + this.innerHTML + '</mjx-body></mjx-doc>';
-      // MathJax.typesetShadow(this.shadowRoot);
-      setTimeout(() => MathJax.typesetShadow(this.shadowRoot), 1);
+    MathText.observer.observe(this, {childList: true});
+  }
+
+  disconnectedCallback() {
+    MathText.observer.disconnect(this, {childList: true});
+  }
+
+}
+
+MathText.contentChanged = (list) => {
+  for (const item of list) {
+    const node = item.target;
+    if (node.isConnected) {
+      node.shadowRoot.firstChild.lastChild.innerHTML = node.innerHTML;
+      setTimeout(() => MathJax.typesetShadow(node.shadowRoot), 1);
+    }
   }
 }
 
-customElements.define('math-text-delayed', MathTextDelayed)
+MathText.observer = new MutationObserver(MathText.contentChanged);
 
+customElements.define('math-text-delayed', MathText);
