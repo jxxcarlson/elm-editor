6f759ddb02c1eb62153361cd3e95f2dd823531cc
James Carlson <jxxcarlson@mac.com>
2020-03-02 23:33:33 +0100

    Make sure that math-text custom elements are ok

--- demo/public/assets/math-text-delayed.js
+++ demo/public/assets/math-text-delayed.js
@@ -1,4 +1,4 @@
-class MathTextDelayed extends HTMLElement {
+class MathText extends HTMLElement {
 
    // The "set content" code below detects the
    // argument to the custom element
@@ -12,10 +12,10 @@ class MathTextDelayed extends HTMLElement {
     this.attachShadow({mode: "open"});
     this.shadowRoot.innerHTML =
       '<mjx-doc><mjx-head></mjx-head><mjx-body>' + this.innerHTML + '</mjx-body></mjx-doc>';
-      // MathJax.typesetShadow(this.shadowRoot);
-      setTimeout(() => MathJax.typesetShadow(this.shadowRoot), 1);
+       // MathJax.typesetShadow(this.shadowRoot)
+       setTimeout(() => MathJax.typesetShadow(this.shadowRoot), 1);
   }
 }
 
-customElements.define('math-text-delayed', MathTextDelayed)
+customElements.define('math-text', MathText)
 
--- demo/public/assets/math-text.js
+++ demo/public/assets/math-text.js
@@ -1,6 +1,6 @@
 class MathText extends HTMLElement {
 
-   // The paragraph below detects the
+   // The "set content" code below detects the
    // argument to the custom element
    // and is necessary for innerHTML
    // to receive the argument.
@@ -12,8 +12,8 @@ class MathText extends HTMLElement {
     this.attachShadow({mode: "open"});
     this.shadowRoot.innerHTML =
       '<mjx-doc><mjx-head></mjx-head><mjx-body>' + this.innerHTML + '</mjx-body></mjx-doc>';
-    MathJax.typesetShadow(this.shadowRoot);
-    // setTimeout(() => MathJax.typesetShadow(this.shadowRoot), 1);
+       MathJax.typesetShadow(this.shadowRoot)
+       // setTimeout(() => MathJax.typesetShadow(this.shadowRoot), 1);
   }
 }
 
