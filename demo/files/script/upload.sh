9da2fdc4d9e018ea762a1e01cbe02f05a8d7af4b
James Carlson <jxxcarlson@gmail.com>
2020-06-03 15:44:11 +0200

    add script

---
+++ demo/files/script/upload.sh
@@ -0,0 +1,2 @@
+scp -r ./src root@161.35.125.40:/root/docserver
+# scp  ./run.sh root@142.93.116.227:/root/docserver
--- demo/src/Config.elm
+++ demo/src/Config.elm
@@ -9,5 +9,10 @@ serverUrl =
     "http://localhost:4000/api"
 
 
+
+--serverUrl =
+--    "http://142.93.116.227:80/api"
+
+
 token =
     "abracadabra"
