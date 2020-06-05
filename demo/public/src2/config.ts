f41e1a84f699e1699416d04f82ac893717b92b7b
James Carlson <jxxcarlson@gmail.com>
2020-06-05 20:09:37 +0200

    Update scripts to run app with server

--- demo/public/src2/config.ts
+++ demo/public/src2/config.ts
@@ -1,4 +1,6 @@
 
-export const FILE_STORE_PATH = "../data"
+const env = Deno.env.toObject();
+
+export const FILE_STORE_PATH = env.MUDOCS || "../data"
 
 export const MANIFEST = FILE_STORE_PATH + '/manifest.yaml'
